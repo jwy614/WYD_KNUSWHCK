51c00a34a829f5e067c86c5bc286b10dbaacd04b
kyr2880 <kkn5839@naver.com>
2021-07-21 01:34:57 +0900

    공지 내용

---
+++ HIMclient/app/src/main/java/com/example/himclient/NoticeDetail.java
@@ -0,0 +1,22 @@
+package com.example.himclient;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoticeDetail extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noticedetail);
+
+
+        }
+    }
+
+
